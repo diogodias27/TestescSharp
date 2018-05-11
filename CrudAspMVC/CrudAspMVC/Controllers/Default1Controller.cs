0867a4cd73643a64682c0bbb542c34bbcb198715
Diogo dias <diogodias_silva18@hotmail.com>
2018-05-11 13:06:43 -0300

    inserção de um crud asp

---
+++ CrudAspMVC/CrudAspMVC/Controllers/Default1Controller.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CrudAspMVC.Models;
+
+namespace CrudAspMVC.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        private readonly List<Produto> todosProdutos;
+
+        public ProdutoController()
+        {
+            todosProdutos = new Produto().TodosProdutos().OrderBy(x => x.Preco).ToList();
+        }
+        public ActionResult Index()
+        {
+            ViewBag.Produtos = "Todos os Produtos";
+            return View(todosProdutos);
+        }
+        public ActionResult Detalhes(int produtoId)
+        {
+            return View(todosProdutos.FirstOrDefault(x => x.ProdutoId == produtoId));
+        }
+        public ActionResult Categorias(string categoria)
+        {
+            var _categoria = todosProdutos.Where(x => x.Categoria == categoria).ToList();
+            ViewBag.Categoria = categoria;
+            return View(_categoria);
+
+        }
+
+    }
+}
---
+++ CrudAspMVC/CrudAspMVC/Models/Produto.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace CrudAspMVC.Models
+{
+    public class Produto
+    {
+        public int ProdutoId { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public string Categoria { get; set; }
+        [DisplayFormat(DataFormatString = "{0:c}")]
+        public decimal Preco { get; set; }
+        [DisplayFormat(DataFormatString = "{0:dd/mm/yyyy}")]
+        public DateTime Data { get; set; }
+
+        public List<Produto> TodosProdutos()
+        {
+            var produtos = new List<Produto>
+            {
+               new Produto
+               {
+                   ProdutoId = 110,
+                   Categoria ="antibiotico",
+                   Nome = "amoxicilina",
+                   Descricao = "indicado para vias aéreas",
+                   Preco = 22.90m,
+                   Data = new DateTime(2012,5,25)
+               },
+               new Produto
+               {
+                   ProdutoId = 120,
+                   Categoria = "antibiotico",
+                   Nome = "cefalexina",
+                   Descricao = "indicado feridas cutaneas",
+                   Preco  = 18.90m,
+                   Data = new DateTime (2013,6,20)
+               },
+               new Produto
+               {
+                ProdutoId = 130,
+                Categoria = "antiflamtorio",
+                Nome = "diclofenaco",
+                Descricao = "indicado para inflamações em geral",
+                Preco = 6.90m,
+                Data = new DateTime (2014,5,15)
+
+
+               },
+
+            };
+            return produtos;
+        }
+    }
+}
