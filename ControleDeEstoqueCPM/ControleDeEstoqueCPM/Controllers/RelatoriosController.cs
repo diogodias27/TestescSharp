0411d30b8226f4e32cd433f4c0f62d5f19a01148
diogo dias silva <diogodias_silva18@hotmail.com>
2019-09-02 22:20:01 -0300

    Aula 4 Como Programar

---
+++ ControleDeEstoqueCPM/ControleDeEstoqueCPM/Controllers/RelatoriosController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ControleDeEstoqueCPM.Controllers
+{
+    public class RelatorioController : Controller
+    {
+        // GET: Relatorios
+        public ActionResult PosicaoEstoque()
+        {
+            return View();
+        }public ActionResult Ressuprimento()
+        {
+            return View();
+        }
+    }
+}
---
+++ ControleDeEstoqueCPM/ControleDeEstoqueCPM/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ControleDeEstoqueCPM.MvcApplication" Language="C#" %>
